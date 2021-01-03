78a23985044784855db809275df65f4aa3e1c7b5
asa1253 <asa1253@live.com>
2021-01-03 16:09:57 +0800

    Update pagic.config.tsx

--- pagic.config.tsx
+++ pagic.config.tsx
@@ -12,71 +12,12 @@ export default {
   github: 'https://github.com/asa1253/asa1253.github.io',
   head: <link rel="icon" type="image/png" href="/favicon.png" />,
   nav: [
-    //{
-    //  text: '加入 QQ 群',
-    //  link: 'https://jq.qq.com/?_wv=1027&k=5nkkFCl',
-    //  target: '_blank',
-    //  popover: (
-    //    <>
-    //      <p style={{ marginTop: 0, marginBottom: '1rem', width: 256 }}>
-    //        一群（767142358）已满，请扫码加二群（706191218）
-    //     </p>
-    //      <img src="/assets/join-qq.jpg" width="256" />
-    //    </>
-    //  ),
-    //},
     {
       text: '本网站使用 Pagic 构建',
       link: 'https://github.com/xcatliu/pagic',
       target: '_blank',
     },
   ],
-  sidebar: {
-    '/': [
-      {
-        link: 'introduction/README.md',
-        children: [
-          'introduction/what-is-typescript.md',
-          'introduction/get-typescript.md',
-          'introduction/hello-typescript.md',
-        ],
-      },
-      {
-        link: 'basics/README.md',
-        children: [
-          'basics/primitive-data-types.md',
-          'basics/any.md',
-          'basics/type-inference.md',
-          'basics/union-types.md',
-          'basics/type-of-object-interfaces.md',
-          'basics/type-of-array.md',
-          'basics/type-of-function.md',
-          'basics/type-assertion.md',
-          'basics/declaration-files.md',
-          'basics/built-in-objects.md',
-        ],
-      },
-      {
-        link: 'advanced/README.md',
-        children: [
-          'advanced/type-aliases.md',
-          'advanced/string-literal-types.md',
-          'advanced/tuple.md',
-          'advanced/enum.md',
-          'advanced/class.md',
-          'advanced/class-and-interfaces.md',
-          'advanced/generics.md',
-          'advanced/declaration-merging.md',
-          'advanced/further-reading.md',
-        ],
-      },
-      {
-        link: 'engineering/README.md',
-        children: ['engineering/lint.md', 'engineering/compiler-options.md'],
-      },
-      'thanks/README.md',
-    ],
-  },
   tools: {
     editOnGitHub: true,
     backToTop: true,
