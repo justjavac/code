ae3273a8f0495b659baf86e81e1a8e2ff6cad677
asa1253 <asa1253@live.com>
2021-01-03 16:13:59 +0800

    1

--- pagic.config.tsx
+++ pagic.config.tsx
@@ -22,34 +22,5 @@ export default {
     editOnGitHub: true,
     backToTop: true,
   },
-  //   tocAd: (
-  //     <div
-  //       dangerouslySetInnerHTML={{
-  //         __html: `
-  // <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  // <!-- 192*128 -->
-  // <ins
-  //   class="adsbygoogle"
-  //   style="display:inline-block;width:192px;height:128px"
-  //   data-ad-client="ca-pub-8483371329009107"
-  //   data-ad-slot="6487368873"
-  // ></ins>
-  // <script>
-  //   (adsbygoogle = window.adsbygoogle || []).push({});
-  // </script>`
-  //       }}
-  //     />
-  //   ),
-  //gitalk: {
-  //  clientID: '29aa4941759fc887ed4f',
-  //  clientSecret: '33e355efdf3a1959624506a5d88311145208471b',
-  //  repo: 'typescript-tutorial',
-  //  owner: 'xcatliu',
-  //  admin: ['xcatliu'],
-  //  pagerDirection: 'first',
-  //},
-  //ga: {
-  //  id: 'UA-45256157-14',
-  //},
   port: 8001,
 };
